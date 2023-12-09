ed476aa25059f995fc3e7431ce2f5e291f76faa7
Felix <kcong.nguyen@icloud.com>
2023-12-09 13:04:40 -0600

    *

--- lua/configs/tokyonight.lua
+++ lua/configs/tokyonight.lua
@@ -1,6 +1,12 @@
 require("tokyonight").setup({
     style = "moon",
     transparent = true,
+    styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+    },
 })
 
 vim.cmd([[colorscheme tokyonight]])

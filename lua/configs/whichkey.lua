1a4d0c57e8de8f4c70fabcd29ae315ad053621dd
Felix <kcong.nguyen@icloud.com>
2023-12-13 19:10:57 -0600

    small change

--- lua/configs/whichkey.lua
+++ lua/configs/whichkey.lua
@@ -18,12 +18,10 @@ keymap("n", "<S-l>", "<CMD>bnext<CR>", opts)
 keymap("n", "<S-h>", "<CMD>bprevious<CR>", opts)
 
 local mappings = {
-  f = {
-    name = "file",
-    f = { "<CMD>Telescope find_files<CR>", "find file" },
-    d = { "<CMD>Telescope live_grep<CR>", "find text" },
-    u = { "<CMD>Telescope undo<CR>", "undo tree" },
-  },
+  f = { "<CMD>Telescope find_files<CR>", "find files" },
+  l = { "<CMD>Telescope live_grep<CR>", "find texts" },
+  u = { "<CMD>Telescope undo<CR>", "undo tree" },
+
   e = { "<CMD>NvimTreeToggle<CR>", "file explorer" },
   w = { "<CMD>Bdelete<CR>", "delete tab" },
   q = { "<CMD>qa<CR>", "quit" },
@@ -31,22 +29,25 @@ local mappings = {
   z = { "<CMD>ZenMode<CR>", "zenmode" },
   o = { "<CMD>TroubleToggle<CR>", "nvim trouble" },
   h = { "<CMD>Glow<CR>", "preview markdown" },
+
   d = {
     name = "diffview",
     o = { "<CMD>DiffviewOpen<CR>", "open" },
     c = { "<CMD>DiffviewClose<CR>", "close" },
   },
+
+  t = { "<CMD>Lspsaga term_toggle<CR>", "terminal" },
+  r = { "<CMD>Lspsaga rename<CR>", "rename" },
+
   a = {
     name = "languages server",
     o = { "<CMD>Lspsaga outline<CR>", "outline" },
-    r = { "<CMD>Lspsaga rename<CR>", "rename" },
     d = { "<CMD>Lspsaga peek_definition<CR>", "definition" },
     k = { "<CMD>Lspsaga diagnostic_jump_prev<CR>", "previous" },
     j = { "<CMD>Lspsaga diagnostic_jump_next<CR>", "next" },
     p = { "<CMD>Lspsaga hover_doc<CR>", "doc" },
     c = { "<CMD>Lspsaga code_action<CR>", "code actions" },
     f = { "<CMD>Lspsaga finder<CR>", "finder" },
-    t = { "<CMD>Lspsaga term_toggle<CR>", "float terminal" },
   },
 }
 
